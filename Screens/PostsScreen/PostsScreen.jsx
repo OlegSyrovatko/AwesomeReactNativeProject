48fc6be6c569a829e3e38dda3351a09a2a10f6d0
Oleg <112899765+OlegSyrovatko@users.noreply.github.com>
2023-06-13 10:58:36 +0300

    add post user

--- Screens/PostsScreen/PostsScreen.jsx
+++ Screens/PostsScreen/PostsScreen.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ImageBackground } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
-import { useNavigation, useRoute } from "@react-navigation/native";
+import { useRoute } from "@react-navigation/native";
+import avatarIcon from "../../assets/avatarIcon.png";
 
 const PostsScreen = ({ navigation }) => {
   const goToLogin = () => {
@@ -30,10 +31,6 @@ const PostsScreen = ({ navigation }) => {
     }
   }
 
-  // const {
-  //   params: { namePhoto, locality, location, urlPhoto },
-  // } = useRoute();
-
   return (
     <View style={styles.container}>
       <MaterialIcons
@@ -44,13 +41,30 @@ const PostsScreen = ({ navigation }) => {
         onPress={goToLogin}
       />
       <Text style={styles.title}>Публікації</Text>
-      <Text>NamePic {NamePic}</Text>
-      <Text>localityPic {localityPic}</Text>
-      <Text>
-        latitude {latitude}
-        longitude {longitude}
-      </Text>
-      <Text>urlPic {urlPic}</Text>
+      <View style={styles.line} />
+      <View style={styles.user}>
+        <ImageBackground
+          source={avatarIcon}
+          resizeMode="cover"
+          style={styles.imageAvatar}
+        ></ImageBackground>
+        <View style={styles.userItems}>
+          <Text style={styles.userName}>Natali Romanova</Text>
+          <Text style={styles.userEml}>email@example.com</Text>
+        </View>
+      </View>
+
+      {/* {urlPic && (
+        <>
+          <Text>NamePic {NamePic}</Text>
+          <Text>localityPic {localityPic}</Text>
+          <Text>
+            latitude {latitude}
+            longitude {longitude}
+          </Text>
+          <Text>urlPic {urlPic}</Text>
+        </>
+      )} */}
     </View>
   );
 };
@@ -80,6 +94,41 @@ const styles = StyleSheet.create({
     left: "89.33%",
     right: "4.27%",
     top: 10,
+  },
+  line: {
+    marginTop: 50,
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    width: "100%",
+  },
+  imageAvatar: {
+    width: 60,
+    height: 60,
+    radius: 16,
+  },
+  user: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 32,
+    marginLeft: 16,
+    gap: 8,
+  },
+  userItems: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  userName: {
+    fontFamily: "Roboto",
+    fontStyle: "Bold",
+    fontSize: 13,
+    lineHeight: 15,
+  },
+  userEml: {
+    fontFamily: "Roboto",
+    fontSize: 11,
+    lineHeight: 13,
+    color: "rgba(33, 33, 33, 0.8)",
   },
 });
 
